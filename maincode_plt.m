a726e81c39d16b80f86df0b9cd47c0e58e9b92b9
akiokaki <akiokaki@umich.edu>
2016-04-19 13:37:36 -0400

    Update maincode_plt.m

--- maincode_plt.m
+++ maincode_plt.m
@@ -12,16 +12,16 @@ data = xlsread('C:\Users\AkioKakishima\Google Drive\Reinforcement_Learning\EWA\e
 
 idx = 1;
     subjects = 1007;
-    for q1 = -5:5:5
-        for q2 = 0.0000001:0.05:1.0000001
+    for q1 = -5:5
+        for q2 = -5:5
             [q,fval]= fminsearch(@fxncode_plt, [q1 q2 0 0 0]);
 
             % learning parameters
             fval;
             lambda = exp(q(1));
-            alpha = exp(q(2));
+            alpha = 1/(1+exp(q(2)));
             IniQ=  [q(3), q(4), q(5)];
-            result(idx,:) = [fval, lambda,alpha, q]
+            result(idx,:) = [fval, lambda,alpha, q];
             %single subject, single block
             save 'singleS_singleB.mat' result;
             idx = idx +1;
